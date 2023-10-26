2733f89fabacb7d29e30110fea3934418838b1bc
Aung Kyaw Nyunt <aungkyawnyunt2004@gmail.com>
2023-10-27 06:05:13 +0700

    [create] article index

---
+++ src/pages/article/index.js
@@ -0,0 +1,15 @@
+import MainLayout from "app/layouts/mainLayout";
+
+import styles from 'app/styles/pages/article.module.css'
+
+export default function Article() {
+  return (
+    <MainLayout>
+      <div className={styles.MainContainer}>
+        <div className={styles.SecondaryContainer}>
+
+        </div>
+      </div>
+    </MainLayout>
+  )
+}
